9d6ae0a6752e830a43b32838a1813be97e31bb00
Michelle Aguayo <22005@portalcedi.edu.mx>
2024-01-30 13:42:29 -0600

    edad

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
- 
+    int x;
+    cout<<"cuantos años tienes?"<<endl;
+    cin>>x;
+    cout<<"la edad es "<<x<<endl;
+    if(x>18){
+        cout<<"Eres mayor de edad"<<endl;
+    }else{
+        cout<<"eres menor de edad"<<endl;
+    }
     return 0;
 }
